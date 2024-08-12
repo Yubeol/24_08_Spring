93879d56a09edd24a29d7617f5fbbd7017be9960
Yubeol <mingu30638@gmail.com>
2024-08-12 11:45:33 +0900

    DB스키마 업데이트

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.Article;
-import com.repository.demo.ArticleRepository;
+import com.example.demo.repository.ArticleRepository;
+import com.exmaple.demo.vo.Article;
 
 @Service
 public class ArticleService {
